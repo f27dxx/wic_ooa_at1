84479130c27f31a78605f5ec2806f17a37a8bbe9
fred <f27dxx@gmail.com>
2020-08-26 20:29:20 +1000

    create employeesController

---
+++ wan_at1_oop/Server/Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using wan_at1_oop.Shared.Models;
+
+namespace wan_at1_oop.Server.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Technician>>> GetAllEmployee()
+        {
+            return await context.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "GetEmployee")]
+        public async Task<ActionResult<Technician>> GetEmployee(int id)
+        {
+            return await context.Employees.FirstOrDefaultAsync(x => x.EmployeeId == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateEmployee(Technician employee)
+        {
+            context.Employees.Add(employee);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("GetEmployee", new { id = employee.EmployeeId }, employee);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateEmployee(Technician employee)
+        {
+            context.Entry(employee).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            var employee = new Technician { EmployeeId = id };
+            context.Employees.Remove(employee);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
