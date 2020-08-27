d5ca42fb17b572865d1a6d1d480875685ad85880
fred <f27dxx@gmail.com>
2020-08-28 03:56:00 +1000

    frontend overhaul ok

---
+++ wan_at1_oop/Server/Controllers/BranchesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using wan_at1_oop.Shared.Models;
+
+namespace wan_at1_oop.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BranchesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public BranchesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Branch>>> GetAllPositions()
+        {
+            return await context.Branches.ToListAsync();
+        }
+    }
+}
---
+++ wan_at1_oop/Server/Controllers/PositionsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using wan_at1_oop.Shared.Models;
+
+namespace wan_at1_oop.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PositionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public PositionsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Position>>> GetAllPositions()
+        {
+            return await context.Positions.ToListAsync();
+        }
+    }
+}
