8bf8de700de36e60f0ccb240953220016956e1af
fred <f27dxx@gmail.com>
2020-09-02 05:46:25 +1000

    validation

--- wan_at1_oop/Shared/Models/Employee.cs
+++ wan_at1_oop/Shared/Models/Employee.cs
@@ -8,15 +8,29 @@ namespace wan_at1_oop.Shared.Models
     {
         [Key]
         public int EmployeeId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required.")]
+        [RegularExpression(@"^[a-zA-Z]*$",
+         ErrorMessage = "Must be english char and no space.")]
+        [MaxLength(20,
+            ErrorMessage = "Must within 20 characters")]
+        [MinLength(3,
+            ErrorMessage = "Must not less than 3 characters")]
         public string EmployeeName { get; set; }
         [Required]
+        [Range(18, 80,
+        ErrorMessage = "{1} - {2}")]
+        [RegularExpression(@"^[\d][\d]\.[\d][\d]$",
+         ErrorMessage = "Incorrect Format")]
         public double RatePerHour { get; set; }
         [ForeignKey("PositionId")]
         [Required]
+        [Range(1, 2,
+        ErrorMessage = "Not valid")]
         public int PositionId { get; set; }
         [ForeignKey("BranchId")]
         [Required]
+        [Range(1, 3,
+        ErrorMessage = "Not valid")]
         public int BranchId { get; set; }
 
 
--- wan_at1_oop/Shared/Models/Technician.cs
+++ wan_at1_oop/Shared/Models/Technician.cs
@@ -6,8 +6,10 @@ namespace wan_at1_oop.Shared.Models
     public class Technician : Employee
     {
         [Required]
+        [RegularExpression(@"^[\dA][\dC][\d][\d][\d][\d]$",
+            ErrorMessage = "Incorrect format")]
         public string Registration { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required.")]
         public string ExpiryDate { get; set; }
     }
 }
